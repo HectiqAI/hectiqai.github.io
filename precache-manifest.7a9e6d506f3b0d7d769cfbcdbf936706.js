ebcb483e07e125388ef90a19a42aa3ed7f67ca5a
Edward Laurence <edward.laurence.1@ulaval.ca>
2020-06-12 16:08:44 -0400

    Updates

---
+++ precache-manifest.7a9e6d506f3b0d7d769cfbcdbf936706.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5ca4f0c0809028b39c8",
+    "url": "/static/js/main.dfde93f8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "772af73320156e4f08cb",
+    "url": "/static/js/2.b0316d8e.chunk.js"
+  },
+  {
+    "revision": "2b2696fe02c3b8a8b516044dd35bf351",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37a0b383421e110facc38f20ef416147.js"
+  "/precache-manifest.7a9e6d506f3b0d7d769cfbcdbf936706.js"
 );
 
 workbox.clientsClaim();
